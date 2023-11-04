2300da89b108e0fc6ee94171483587f94268741f
Rajdeep Singh <radewala.2@gmail.com>
2023-11-04 19:30:19 +0530

    add meta data for seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,34 @@
 import '../globals.css'
+import { Header } from "@/components/Header/Header"
+import { Metadata } from 'next'
+import { absoluteUrl } from '@/lib/utils'
+
+export const metadata: Metadata = {
+  title: {
+    default: 'Outstatic',
+    template: '%s | Outstatic'
+  },
+  description: 'A blog starter built with Outstatic.',
+  openGraph: {
+    title: 'Outstatic - A Static Site CMS for Next.js',
+    description: 'A blog starter built with Outstatic.',
+    url: absoluteUrl('/'),
+    siteName: 'Next.js',
+    images: [
+      {
+        url: absoluteUrl('/images/og-image.png'),
+        width: 1800,
+        height: 1600
+      }
+    ],
+    locale: 'en_US',
+    type: 'website'
+  },
+  icons: {
+    icon: [{ url: '/favicon.ico' }],
+    apple: [{ url: '/favicon.ico' }]
+  }
+}
 
 export default function RootLayout({
   children
@@ -7,7 +37,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body data-new-gr-c-s-check-loaded={true} data-gr-ext-installed={true} className='bg-white dark:text-white dark:bg-gray-900'>
+
+        <Header />
+
+        {children}
+
+      </body>
     </html>
   )
 }
