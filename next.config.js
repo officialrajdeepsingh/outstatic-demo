82673c4db5fa5824161cb21169b8b80453d5be8a
Rajdeep Singh <radewala.2@gmail.com>
2023-11-09 14:01:25 +0530

    remove the image host name

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   images: {
-    unoptimized: true,
-    // remotePatterns: [
-    //   {
-    //     hostname: 'avatars.githubusercontent.com',
-    //   },
-    // ],
+    remotePatterns: [
+      {
+        hostname: 'avatars.githubusercontent.com',
+      },
+    ],
   }
 }
 
